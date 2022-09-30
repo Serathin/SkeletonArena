6a060251b95d97492b8b14d77aaad32ee096d069
Serathin <Lebedev97@bk.ru>
2022-09-30 22:50:04 +0300

    Add auto heal to health component Also rebuild lighting

--- Source/SkeletonArena/Private/Components/SAHealthComponent.cpp
+++ Source/SkeletonArena/Private/Components/SAHealthComponent.cpp
@@ -3,6 +3,8 @@
 
 #include "Components/SAHealthComponent.h"
 #include <GameFramework/Actor.h>
+#include <Engine/World.h>
+#include <TimerManager.h>
 
 #include "Dev/SAPoisonDamageType.h"
 #include "Dev/SABleedingDamageType.h"
@@ -10,6 +12,10 @@
 USAHealthComponent::USAHealthComponent()
 :
 max_health_(100.f)
+, auto_heal_enabled_(true)
+, auto_heal_delay_(2.f)
+, auto_heal_update_frequency_(0.3f)
+, auto_heal_amount_(1.f)
 , health_(max_health_)
 {
 	PrimaryComponentTick.bCanEverTick = false;
@@ -19,16 +25,28 @@ void USAHealthComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-	health_ = max_health_;
-	on_health_changed_.Broadcast(health_);
+	SetHealth(max_health_);
 
 	if (AActor *owner = GetOwner()) owner->OnTakeAnyDamage.AddDynamic(this, &USAHealthComponent::OnTakeAnyDamage);
 }
 
 void USAHealthComponent::OnTakeAnyDamage(AActor* DamagedActor, float Damage, const class UDamageType* DamageType, class AController* InstigatedBy, AActor* DamageCauser)
 {
-	if (Damage < 0.f || IsDead()) return;
-	health_ = FMath::Clamp(health_ - Damage, 0.f, max_health_);
+	if (Damage < 0.f || IsDead() || !GetWorld()) return;
+	SetHealth(health_ - Damage);
+	GetWorld()->GetTimerManager().ClearTimer(auto_heal_timer_);
+	if (IsDead()) on_death_.Broadcast();
+	else if (auto_heal_enabled_) GetWorld()->GetTimerManager().SetTimer(auto_heal_timer_, this, &USAHealthComponent::AutoHeal, auto_heal_update_frequency_, true, auto_heal_delay_);
+}
+
+void USAHealthComponent::AutoHeal()
+{
+	if (!IsDead()) SetHealth(health_ + auto_heal_amount_);
+}
+
+void USAHealthComponent::SetHealth(float health)
+{
+	health_ = FMath::Clamp(health, 0.f, max_health_);
 	on_health_changed_.Broadcast(health_);
-	if (health_ <= 0.f) on_death_.Broadcast();
+	if (FMath::IsNearlyEqual(health_, max_health_) && GetWorld()) GetWorld()->GetTimerManager().ClearTimer(auto_heal_timer_);
 }
--- Source/SkeletonArena/Public/Components/SAHealthComponent.h
+++ Source/SkeletonArena/Public/Components/SAHealthComponent.h
@@ -14,12 +14,11 @@ class SKELETONARENA_API USAHealthComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
-public:	
-	// Sets default values for this component's properties
+public:
 	USAHealthComponent();
 
 	float GetHealth() const { return health_; }
-	bool IsDead() const { return health_ <= 0.f; }
+	bool IsDead() const { return FMath::IsNearlyZero(health_); }
 
 	FOnDeath on_death_;
 	FOnHealthChanged on_health_changed_;
@@ -31,9 +30,26 @@ protected:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, meta = (ClampMin = 0.f, ClampMax = 1000000.f))
 	float max_health_;
 
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = AutoHeal)
+	bool auto_heal_enabled_;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = AutoHeal, meta = (ClampMin = 0.f, EditCondition = auto_heal_enabled_))
+	float auto_heal_delay_;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = AutoHeal, meta = (ClampMin = 0.f, EditCondition = auto_heal_enabled_))
+	float auto_heal_update_frequency_;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AutoHeal, meta = (ClampMin = 0.f, EditCondition = auto_heal_enabled_))
+	float auto_heal_amount_;
+
 private:
 	UFUNCTION()
   	void OnTakeAnyDamage(AActor* DamagedActor, float Damage, const class UDamageType* DamageType, class AController* InstigatedBy, AActor* DamageCauser);
 
+	void AutoHeal();
+	void SetHealth(float health);
+
 	float health_;
+
+	FTimerHandle auto_heal_timer_;
 };
